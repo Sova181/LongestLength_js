84549e89235d46e5e09b1b0a90a3472d07ff0b73
Таня Дашипилова <tanya.volk01@mail.ru>
2024-05-23 08:46:07 +0800

    +

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const func = require('./func_long');
 
-console.log(func('qweqrty')); // 6 (подстрока weqrty)
-console.log(func('abdfehueily')); //  6 (подстрока dfehue)
-console.log(func('')); //0 (пустая строка)
-console.log(func('abcdeef')); // 5 abcde
+console.log(func('qweqrty')); // 6
+console.log(func('abdfehueily')); // 6
+console.log(func('')); //0 
+console.log(func('abcdeef')); // 5 
