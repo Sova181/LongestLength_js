ce98e2e91e77bbedca646c3fec6329c648bb0238
Таня Дашипилова <tanya.volk01@mail.ru>
2024-05-23 08:47:15 +0800

    исправила

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const func = require('./func_long');
 
 console.log(func('qweqrty')); // 6
-console.log(func('abdfehueily')); // 6
+console.log(func('abdfehueily')); // 7
 console.log(func('')); //0 
 console.log(func('abcdeef')); // 5 
